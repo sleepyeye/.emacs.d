0e67cdf3d264cd538ee92b8cbfdc4c8910dcf230
sleepyeye <wonjunlee.0729@gmail.com>
2023-01-18 20:14:27 +0900

    add git/ui related packages

--- magit.el
+++ magit.el
@@ -4,3 +4,12 @@
   (sleepy/util-keys
    "g" 'magit-status)
   )
+
+
+(elpaca-use-package git-gutter
+  :demand t
+  :config
+  (global-git-gutter-mode +1))
+
+
+(elpaca-use-package git-timemachine)
--- ui.el
+++ ui.el
@@ -10,3 +10,22 @@
 
 ;; theme
 (load-theme 'modus-operandi)            ; Light theme
+
+(elpaca-use-package pulsar
+  :init
+  :hook
+  ((consult-after-jump-hook . pulsar-recenter-top)
+   (consult-after-jump-hook . pulsar-reveal-entry))
+  :config
+  (setq pulsar-pulse t)
+  (setq pulsar-delay 0.055)
+  (setq pulsar-iterations 10)
+  (setq pulsar-face 'pulsar-magenta)
+  (setq pulsar-highlight-face 'pulsar-yellow)
+  (pulsar-global-mode 1))
+
+(elpaca-use-package hl-todo
+  :demand t
+  :config
+  (global-hl-todo-mode))
+  
