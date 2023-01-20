178e29f583a9585fc322b30492e099d7a0c85376
sleepyeye <wonjunlee.0729@gmail.com>
2023-01-20 12:19:41 +0900

    add more mode hook to lsp

--- lsp.el
+++ lsp.el
@@ -3,12 +3,19 @@
   (setq lsp-keymap-prefix "C-c l")
   (add-hook 'lsp-mode-hook #'evil-normalize-keymaps)
   :hook ((latex-mode . lsp)
+	 (python-mode . lsp)
+	 (python-ts-mode . lsp)
+	 (c-mode . lsp)
+	 (c-ts-mode . lsp)
+	 (c++-mode . lsp)
+	 (c++-ts-mode . lsp)
+	 (cmake-ts-mode . lsp)
 	 (lsp-mode . lsp-enable-which-key-integration))
   :general
   (general-evil-define-key '(normal) lsp-mode-map
     "gd" #'lsp-find-definition
     "gD" #'lsp-find-references)
-  :commands lsp)
+  :commands (lsp lsp-deferred))
 
 ;; (elpaca-use-package consult-lsp
 ;;   :defer t
