e4064f52db64273ad9b4dc9db09f673f9ddf5c32
sleepyeye <wonjunlee.0729@gmail.com>
2023-01-23 18:00:59 +0900

    Import treesit-auto

--- tree-sitter.el
+++ tree-sitter.el
@@ -1,32 +1,12 @@
-;;; remap tree-stitter supported modes
-(push '(python-mode . python-ts-mode) major-mode-remap-alist)
-(push '(css-mode . css-ts-mode) major-mode-remap-alist)
-(push '(javascript-mode . js-ts-mode) major-mode-remap-alist)
-(push '(js-json-mode . json-ts-mode) major-mode-remap-alist)
-(push '(typescript-mode . typescript-ts-mode) major-mode-remap-alist)
-(push '(c-mode . c-ts-mode) major-mode-remap-alist)
-(push '(c++-mode . c++-ts-mode) major-mode-remap-alist)
-
-
-
-
 ;;; Emacs built-in treesitter package
 (use-feature treesit
   :demand t
-  :init
   :config
-  (add-to-list 'treesit-language-source-alist
-	       '(c "https://github.com/tree-sitter/tree-sitter-c.git"))
-  (add-to-list 'treesit-language-source-alist
-	       '(c++ "https://github.com/tree-sitter/tree-sitter-cpp.git"))
-  (add-to-list 'treesit-language-source-alist
-	       '(rust "https://github.com/tree-sitter/tree-sitter-rust.git"))
-  (add-to-list 'treesit-language-source-alist
-	       '(cmake "https://github.com/uyha/tree-sitter-cmake.git"))
-  (add-to-list 'treesit-language-source-alist
-	       '(python "https://github.com/tree-sitter/tree-sitter-python.git"))
-     
-  (setq treesit-font-lock-level 4))
+  (setq treesit-font-lock-level 4)
+  ;; this package is not in MELPA
+  ;; credit @renzmann
+  (load "~/.emacs.d/treesit-auto.el"))
+
 
 (elpaca-use-package evil-textobj-tree-sitter
   :after (treest evil)
