633b6c2326b15996f5974c23b43a262df5d6f381
sleepyeye <wonjunlee.0729@gmail.com>
2023-01-26 11:24:01 +0900

    Update completion rules

--- completion.el
+++ completion.el
@@ -42,11 +42,14 @@
   :demand t
   :custom
   (completion-styles '(orderless basic))
+  (completion-ignore-case t)
+  (completion-category-defaults nil)
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (elpaca-use-package marginalia
   :demand t
   :init
+  (setq marginalia-annotators-heavy t)
   (marginalia-mode))
 
 (elpaca-use-package (corfu :files (:defaults "extensions/*"))
