03af470f9f3f4f2b9dc986a5b73cdf0cc2837254
sleepyeye <wonjunlee.0729@gmail.com>
2023-02-01 20:52:18 +0900

    Upate cc.el

--- cc.el
+++ cc.el
@@ -1,8 +1,18 @@
-(elpaca-use-package cmake-font-lock)
+(use-feature cmake-mode
+  :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
+
+(elpaca-use-package cmake-font-lock
+  :after (cmake-mode)
+  :hook (cmake-mode . cmake-font-lock-activate))
+
 
 (elpaca-use-package modern-cpp-font-lock
   :config
   (modern-c++-font-lock-global-mode t))
+
+(elpaca-use-package google-c-style
+  :hook ((c-mode c++-mode) . google-set-c-style)
+  (c-mode-common . google-make-newline-indent))
 
 
 (add-hook 'c-mode-hook 'lsp)
