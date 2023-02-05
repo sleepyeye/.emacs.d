7ca95fcc85afa363fd247026416a5927a432a553
sleepyeye <wonjunlee.0729@gmail.com>
2023-02-05 17:17:44 +0900

    update lsp setup

--- lsp.el
+++ lsp.el
@@ -1,27 +1,47 @@
 (elpaca-use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  (add-hook 'lsp-mode-hook #'evil-normalize-keymaps)
-  :hook ((latex-mode . lsp)
-	 (python-mode . lsp)
-	 (python-ts-mode . lsp)
-	 (c-mode . lsp)
-	 (c-ts-mode . lsp)
-	 (c++-mode . lsp)
-	 (c++-ts-mode . lsp)
-	 (cmake-ts-mode . lsp)
-	 (lsp-mode . lsp-enable-which-key-integration))
-  :general
-  (general-evil-define-key '(normal) lsp-mode-map
-    "gd" #'lsp-find-definition
-    "gD" #'lsp-find-references)
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-diagnostics-provider :none)
-  (setq lsp-ui-sideline-enable nil)
-  (setq lsp-eldoc-enable-hover nil)
+	:init
+	(setq lsp-keymap-prefix "C-c l")
+	(add-hook 'lsp-mode-hook #'evil-normalize-keymaps)
+	:hook ((latex-mode . lsp-deferred)
+				 (python-mode . lsp-deferred)
+				 (python-ts-mode . lsp-deferred)
+				 (c-mode . lsp-deferred)
+				 (c-ts-mode . lsp-deferred)
+				 (c++-mode . lsp-deferred)
+				 (c++-ts-mode . lsp-deferred)
+				 (cmake-ts-mode . lsp-deferred)
+				 (lsp-mode . lsp-enable-which-key-integration))
+	:general
+	(general-evil-define-key '(normal) lsp-mode-map
+		"gd" #'lsp-find-definition
+		"gD" #'lsp-find-references)
+
+
+	(sleepy/code-keys "r" #'lsp-rename)
+		
+	:commands (lsp lsp-deferred)
+	:config
+	(setq lsp-diagnostics-provider :none)
+	(setq lsp-ui-sideline-enable nil)
+	(setq lsp-eldoc-enable-hover nil)
+	(setq lsp-modeline-diagnostics-enable nil)
+
+  (setq lsp-auto-guess-root t)
+  (setq lsp-log-io nil)
+  (setq lsp-restart 'auto-restart)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-modeline-code-actions-enable nil)
   (setq lsp-modeline-diagnostics-enable nil)
-  )
+  ;; (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-semantic-tokens-enable nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-imenu nil)
+  (setq lsp-enable-snippet nil)
+  (setq lsp-idle-delay 0.5))
 
 ;; (elpaca-use-package consult-lsp
 ;;   :defer t
