e23d07b91c5d034a1e0aad1839a9ca654a979cad
sleepyeye <wonjunlee.0729@gmail.com>
2023-02-22 01:44:10 +0900

    add Tempel mode

--- init.el
+++ init.el
@@ -28,3 +28,43 @@
 (load "~/.emacs.d/cc.el")
 ;; (load "~/.emacs.d/python.el")
 
+
+;; Configure Tempel
+(elpaca-use-package tempel
+	;; Require trigger prefix before template name when completing.
+	:custom
+	(tempel-trigger-prefix
+	 "<")
+
+	:bind (("M-." . tempel-complete) ;; Alternative tempel-expand
+				 ("s-." . tempel-complete)
+				 ("M-," . tempel-insert)
+				 ("s-," . tempel-insert))
+	
+	:init
+	;; Setup completion at point
+	(defun tempel-setup-capf ()
+		;; Add the Tempel Capf to `completion-at-point-functions'.
+		;; `tempel-expand' only triggers on exact matches. Alternatively use
+		;; `tempel-complete' if you want to see all matches, but then you
+		;; should also configure `tempel-trigger-prefix', such that Tempel
+		;; does not trigger too often when you don't expect it. NOTE: We add
+		;; `tempel-expand' *before* the main programming mode Capf, such
+		;; that it will be tried first.
+		(setq-local completion-at-point-functions
+								(cons #'tempel-expand
+											completion-at-point-functions)))
+	
+	(add-hook 'prog-mode-hook 'tempel-setup-capf)
+	(add-hook 'text-mode-hook 'tempel-setup-capf)
+
+	;; Optionally make the Tempel templates available to Abbrev,
+	;; either locally or globally. `expand-abbrev' is bound to C-x '.
+	(add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+	(global-tempel-abbrev-mode)
+	:config
+	(defvar sleepy/tempel-templates
+		'((templates "Global template"))
+		"My templates.")
+	(add-to-list 'tempel-template-sources 'sleepy/tempel-templates)
+	)
