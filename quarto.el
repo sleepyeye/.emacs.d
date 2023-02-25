38d72af4ab9edfaad2e8dc4d2c4b95dcc787d107
sleepyeye <wonjunlee.0729@gmail.com>
2023-02-26 01:08:40 +0900

    update quarto keymap

--- quarto.el
+++ quarto.el
@@ -1,4 +1,7 @@
 (elpaca-use-package quarto-mode
-  :mode (("\\.Rmd" . poly-quarto-mode)))
+  :mode (("\\.Rmd" . poly-quarto-mode))
+	:config
+	(setq quarto-preview-display-buffer nil)
+	(define-key poly-quarto-mode-map (kbd "C-c C-c") 'quarto-preview))
 
   
