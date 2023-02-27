0ec77cea2d88ed441547fffe3d4b261b7140e528
sleepyeye <wonjunlee.0729@gmail.com>
2023-02-27 14:56:14 +0900

    Update python configs

--- python.el
+++ python.el
@@ -1,12 +1,22 @@
 (use-package python
-  :after projectile 
-  :init
-  (setq python-indent-guess-indent-offset-verbose nil)
-  :config
-  (when (and (executable-find "python3")
-	     (string= python-shell-interpreter "python"))
-    (setq python-shell-interpreter "python3"))
-  ))
+	:after projectile 
+	:init
+	(setq python-indent-guess-indent-offset-verbose nil)
+	:config
+	(when (and (executable-find "python3")
+						 (string= python-shell-interpreter "python"))
+		(setq python-shell-interpreter "python3")))
 
+(use-package python-black
+	:after python
+	:hook ((python-mode . python-black-on-save-mode-enable-dwim)
+				 (python-ts-mode . python-black-on-save-mode-enable-dwim))
+	:config
+	(general-evil-define-key '(normal) python-ts-mode-map
+		"=" #'python-black-region)
 
+	(general-evil-define-key '(normal) python-mode-map
+		"=" #'python-black-region))
+
+;; (elpaca-use-package lsp-pyright)
 
