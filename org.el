6caccbdb353d8e542ed08ca26403c58fba79134d
sleepyeye <wonjunlee.0729@gmail.com>
2023-03-04 18:07:52 +0900

    Add org-contrib and doct

--- org.el
+++ org.el
@@ -1,4 +1,9 @@
 (use-package org
   :mode ("\\.org" . org-mode))
 
+(use-package org-contrib
+	:defer 10)
 
+(use-package doct
+  :elpaca (doct :branch "development" :protocol ssh :depth nil)
+  :commands (doct))
