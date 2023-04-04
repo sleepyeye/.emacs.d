829c80ebcbdc9fe2456c6b36014bb9dc8c634281
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-04 22:43:28 +0900

    Apply formatting

--- completion.el
+++ completion.el
@@ -1,56 +1,56 @@
 (use-package vertico
-	:demand t
-	:init
-	(vertico-mode)
-	(setq vertico-count 10
-				vertico-cycle t))
+  :demand t
+  :init
+  (vertico-mode)
+  (setq vertico-count 10
+		vertico-cycle t))
 
 (use-package consult
   :demand t
   :config
   (sleepy/leader-def
-    "bd" 'kill-current-buffer
-    "br" 'revert-buffer
-    "bb" 'consult-buffer
-    "bB" 'consult-buffer-other-window
-    "bp" 'consult-project-buffer)
+	"bd" 'kill-current-buffer
+	"br" 'revert-buffer
+	"bb" 'consult-buffer
+	"bB" 'consult-buffer-other-window
+	"bp" 'consult-project-buffer)
 
   ;; TODO fix name
   (defun consult-ripgrep-current ()
-    (interactive)
-    (consult-ripgrep default-directory))
+	(interactive)
+	(consult-ripgrep default-directory))
 
   (sleepy/leader-def
-    "si" 'consult-imenu
-    "sb" 'consult-line
-    "sB" 'consult-line-multi
-    "sd" 'consult-ripgrep-current
-    "sp" 'consult-ripgrep
-    "sg" 'consult-git-grep)
+	"si" 'consult-imenu
+	"sb" 'consult-line
+	"sB" 'consult-line-multi
+	"sd" 'consult-ripgrep-current
+	"sp" 'consult-ripgrep
+	"sg" 'consult-git-grep)
 
   (sleepy/leader-def
-    "ji" 'consult-outline
-    "jl" 'consult-goto-line
-    "jo" 'consult-outline
-    "jd" 'consult-ripgrep
-    "jg" 'consult-git-grep)
+	"ji" 'consult-outline
+	"jl" 'consult-goto-line
+	"jo" 'consult-outline
+	"jd" 'consult-ripgrep
+	"jg" 'consult-git-grep)
 
   (sleepy/leader-def
-    "uy" 'consult-yank-pop)
+	"uy" 'consult-yank-pop)
 
   (sleepy/leader-def
-    "fr" 'consult-recent-file)
+	"fr" 'consult-recent-file)
 
   :config
-	;; Turn off live-preview
-	(consult-customize
-	 consult-theme
-	 consult-recent-file
-	 :preview-key nil)
+  ;; Turn off live-preview
+  (consult-customize
+   consult-theme
+   consult-recent-file
+   :preview-key nil)
 
 
   (setq xref-show-xrefs-function #'consult-xref
-				xref-show-definitions-function #'consult-xref))
+		xref-show-definitions-function #'consult-xref))
 
 (use-package orderless
   :demand t
@@ -78,7 +78,7 @@
   (corfu-quit-at-boundary 'separator)
   (corfu-quit-no-match t)
   (corfu-scroll-margin 5)
-	(corfu-max-width 50)
+  (corfu-max-width 50)
   :bind
   (:map corfu-map ("M-SPC" . corfu-insert-separator))
   :init
@@ -88,10 +88,10 @@
 (use-package cape
   :init
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-	;; FIXME set these cape-XXX as mode local
+  ;; FIXME set these cape-XXX as mode local
   ;; (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
   (add-to-list 'completion-at-point-functions #'cape-symbol)
   ;; (add-to-list 'completion-at-point-functions #'cape-tex)
-   )
+  )
 
