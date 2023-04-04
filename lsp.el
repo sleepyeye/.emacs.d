2494089022b28152bd0a5cf7d5f3756dad4d44bc
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-04 23:32:12 +0900

    Format

--- lsp.el
+++ lsp.el
@@ -1,41 +1,41 @@
 (use-package lsp-mode
-	:init
-	(setq lsp-keymap-prefix "C-c l")
-	(add-hook 'lsp-mode-hook #'evil-normalize-keymaps)
-	:hook ((latex-mode . lsp-deferred)
-				 (python-mode . lsp-deferred)
-				 (python-ts-mode . lsp-deferred)
-				 (c-mode . lsp-deferred)
-				 (c-ts-mode . lsp-deferred)
-				 (c++-mode . lsp-deferred)
-				 (c++-ts-mode . lsp-deferred)
-				 (cmake-mode . lsp-deferred)
-				 (cmake-ts-mode . lsp-deferred)
-				 (lsp-mode . lsp-enable-which-key-integration))
-	:config
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  (add-hook 'lsp-mode-hook #'evil-normalize-keymaps)
+  :hook ((latex-mode . lsp-deferred)
+		 (python-mode . lsp-deferred)
+		 (python-ts-mode . lsp-deferred)
+		 (c-mode . lsp-deferred)
+		 (c-ts-mode . lsp-deferred)
+		 (c++-mode . lsp-deferred)
+		 (c++-ts-mode . lsp-deferred)
+		 (cmake-mode . lsp-deferred)
+		 (cmake-ts-mode . lsp-deferred)
+		 (lsp-mode . lsp-enable-which-key-integration))
+  :config
 
-	(general-evil-define-key '(normal) lsp-mode-map
-		"gd" #'lsp-find-definition
-		"gD" #'lsp-find-references
-		"g=" #'lsp-format-buffer)
+  (general-evil-define-key '(normal) lsp-mode-map
+	"gd" #'lsp-find-definition
+	"gD" #'lsp-find-references
+	"g=" #'lsp-format-buffer)
 
-	(general-evil-define-key '(normal visual motion) lsp-mode-map
-		"=" #'lsp-format-region)
+  (general-evil-define-key '(normal visual motion) lsp-mode-map
+	"=" #'lsp-format-region)
 
 
-	(sleepy/leader-def "cr" #'lsp-rename)
-		
-	:commands (lsp lsp-deferred)
-	:config
-	(setq lsp-completion-provider :none)
-	(defun corfu-lsp-setup ()
-		(setq-local completion-styles '(orderless)
-								completion-category-defaults nil))
-	(add-hook 'lsp-mode-hook #'corfu-lsp-setup)
-	(setq lsp-diagnostics-provider :none)
-	(setq lsp-ui-sideline-enable nil)
-	(setq lsp-eldoc-enable-hover nil)
-	(setq lsp-modeline-diagnostics-enable nil)
+  (sleepy/leader-def "cr" #'lsp-rename)
+  
+  :commands (lsp lsp-deferred)
+  :config
+  (setq lsp-completion-provider :none)
+  (defun corfu-lsp-setup ()
+	(setq-local completion-styles '(orderless)
+				completion-category-defaults nil))
+  (add-hook 'lsp-mode-hook #'corfu-lsp-setup)
+  (setq lsp-diagnostics-provider :none)
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-eldoc-enable-hover nil)
+  (setq lsp-modeline-diagnostics-enable nil)
 
   (setq lsp-auto-guess-root t)
   (setq lsp-log-io nil)
