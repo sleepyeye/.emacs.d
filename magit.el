da360e40a919ec94556ca1b56d8e28155f048298
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-04 22:57:20 +0900

    Update magit and git-gutter-fringe setup

--- magit.el
+++ magit.el
@@ -2,11 +2,13 @@
   :demand t
   :config
   (sleepy/leader-def
-   "gg" 'magit-status)
+	"gg" 'magit-status)
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
-				magit-diff-hide-trailing-cr-characters t)
+		magit-diff-hide-trailing-cr-characters t
+		magit-save-window-configuration 'dontask)
   )
+
 
 
 
@@ -22,20 +24,20 @@
 		git-gutter:window-width 1
 		git-gutter:ask-p nil))
 
-;; (use-package git-gutter-fringe
-;;   :diminish git-gutter-mode
-;;   :after git-gutter
-;;   :demand fringe-helper
-;; 	:config
-;;   ;; subtle diff indicators in the fringe
-;;   ;; places the git gutter outside the margins.
-;;   (setq-default fringes-outside-margins t)
-;; 	)
+(use-package git-gutter-fringe
+  :diminish git-gutter-mode
+  :after git-gutter
+  :demand fringe-helper
+  :config
+  ;; subtle diff indicators in the fringe
+  ;; places the git gutter outside the margins.
+  (setq-default fringes-outside-margins t)
+  )
 
 
 (use-package git-timemachine
   :commands git-timemachine
   :config
   (sleepy/leader-def
-   "gt" 'git-timemachine))
+	"gt" 'git-timemachine))
 
