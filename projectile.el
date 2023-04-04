1f8e97c2a5903698cb863892e13eb7abf3f961b0
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-04 22:42:08 +0900

    Apply formatting

--- projectile.el
+++ projectile.el
@@ -1,37 +1,33 @@
 (use-package projectile
   :demand t
   :after (general)
-	:config
-
+  :config
   ;;; override the keymap
   (sleepy/leader-def
-    "SPC" 'projectile-find-file)
+	"SPC" 'projectile-find-file)
 
   (sleepy/leader-def
-    "p!" 'projectile-run-shell-command-in-root
-    "p%" 'projectile-replace-regexp
-    "p&" 'projectile-run-async-shell-command-in-root
-    "p-" 'projectile-dired
-    "pf" 'projectile-find-file
-    "ps" 'projectile-rg
-    "pc" 'projectile-compile-project
-    "pC" 'projectile-configure-project
-    "pe" 'projectile-edit-dir-locals
-    "pI" 'projectile-invalidate-cache
-    "pk" 'projectile-kill-buffers
-    "pR" 'projectile-replace
-    "pS" 'projectile-save-project-buffers
-    "pv" 'projectile-vc)
+	"p!" 'projectile-run-shell-command-in-root
+	"p%" 'projectile-replace-regexp
+	"p&" 'projectile-run-async-shell-command-in-root
+	"p-" 'projectile-dired
+	"pf" 'projectile-find-file
+	"ps" 'projectile-rg
+	"pc" 'projectile-compile-project
+	"pC" 'projectile-configure-project
+	"pe" 'projectile-edit-dir-locals
+	"pI" 'projectile-invalidate-cache
+	"pk" 'projectile-kill-buffers
+	"pR" 'projectile-replace
+	"pS" 'projectile-save-project-buffers
+	"pv" 'projectile-vc)
   :config
   (projectile-mode)
-	(add-to-list 'projectile-globally-ignored-directories "^\\build$")
-	(add-to-list 'projectile-globally-ignored-directories "^\\elpa$")
-	(add-to-list 'projectile-globally-ignored-directories "^\\url$")
-	(setq projectile-enable-caching t)
-	(setq projectile-require-project-root t)
-
-
-	)
+  (add-to-list 'projectile-globally-ignored-directories "^\\build$")
+  (add-to-list 'projectile-globally-ignored-directories "^\\elpa$")
+  (add-to-list 'projectile-globally-ignored-directories "^\\url$")
+  (setq projectile-enable-caching t)
+  (setq projectile-require-project-root t))
 
 
 (use-package consult-projectile)
