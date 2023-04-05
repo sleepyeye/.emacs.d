da2b5d3255d774905bd6aab5523bdbbb605695c4
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-05 12:49:14 +0900

    Turn on cape-file capf

--- completion.el
+++ completion.el
@@ -89,7 +89,7 @@
   :init
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   ;; FIXME set these cape-XXX as mode local
-  ;; (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
   (add-to-list 'completion-at-point-functions #'cape-symbol)
   ;; (add-to-list 'completion-at-point-functions #'cape-tex)
