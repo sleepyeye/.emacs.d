dca3b31de66b0d48bfe612d220e0210b069dc6ec
sleepyeye <wonjunlee.0729@gmail.com>
2023-04-21 01:15:29 +0900

    Enable dwim-shell-command only in macos

--- dwim-shell-command.el
+++ dwim-shell-command.el
@@ -1,5 +1,6 @@
 (use-package dwim-shell-command
   :defer 3
+  :if (memq window-system '(mac ns))
   :bind (([remap shell-command] . dwim-shell-command)
 		 :map dired-mode-map
 		 ([remap dired-do-async-shell-command] . dwim-shell-command)
