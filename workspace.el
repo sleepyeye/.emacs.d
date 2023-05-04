370971b907ba4dfb028fe178b40caea713ef9e05
sleepyeye <wonjunlee.0729@gmail.com>
2023-05-04 13:40:46 +0900

    Disable buffer source from perspective.el

--- workspace.el
+++ workspace.el
@@ -23,10 +23,17 @@
 
   :custom
   (persp-mode-prefix-key (kbd "C-c M-p"))
-  :config
-  (consult-customize consult--source-buffer :hidden t :default nil)
-  (add-to-list 'consult-buffer-sources persp-consult-source)
+ ;; :config
+  ;; (consult-customize consult--source-buffer :hidden t :default nil)
+  ;; ;; (add-to-list 'consult-buffer-sources persp-consult-source)
+  ;; (defvar consult--source-perspective
+  ;; 	(list :name     "Perspective"
+  ;; 		  :narrow   ?s
+  ;; 		  :category 'buffer
+  ;; 		  :state    #'consult--buffer-state
+  ;; 		  :default  t
+  ;; 		  :items    #'persp-get-buffer-names))
+
+  ;; (push consult--source-perspective consult-buffer-sources)
   :init
   (persp-mode))
-
-
