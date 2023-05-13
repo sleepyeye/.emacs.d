ce732bc11fea5c1cf1108947c298e99fc46fc075
sleepyeye <wonjunlee.0729@gmail.com>
2023-05-13 09:31:46 +0900

    Update peerspective

--- workspace.el
+++ workspace.el
@@ -1,9 +1,18 @@
 (use-package perspective
   :demand t
   :after consult
+  :init
+  (setq persp-state-default-file (expand-file-name ".persp" user-emacs-directory))
+
   :config
   (sleepy/leader-def
     "TAB TAB" #'persp-switch
+    "TAB `" 'persp-switch-last
+    "TAB d" 'persp-kill
+    "TAB h" 'persp-prev
+    "TAB l" 'persp-next
+    "TAB x" '((lambda () (interactive) (persp-kill (persp-current-name))) :wk "kill current")
+    "TAB X" '((lambda () (interactive) (persp-kill (persp-names))) :wk "kill all")
     "TAB [" #'persp-prev
     "TAB ]" #'persp-next
     "TAB s" #'persp-save
@@ -20,20 +29,6 @@
     "TAB d" #'persp-kill-buffer*
     "TAB D" #'persp-kill-others)
 
-
-  :custom
-  (persp-mode-prefix-key (kbd "C-c M-p"))
- ;; :config
-  ;; (consult-customize consult--source-buffer :hidden t :default nil)
-  ;; ;; (add-to-list 'consult-buffer-sources persp-consult-source)
-  ;; (defvar consult--source-perspective
-  ;; 	(list :name     "Perspective"
-  ;; 		  :narrow   ?s
-  ;; 		  :category 'buffer
-  ;; 		  :state    #'consult--buffer-state
-  ;; 		  :default  t
-  ;; 		  :items    #'persp-get-buffer-names))
-
-  ;; (push consult--source-perspective consult-buffer-sources)
   :init
-  (persp-mode))
+  (persp-mode)
+  (add-hook 'kill-emacs-hook #'persp-state-save))
