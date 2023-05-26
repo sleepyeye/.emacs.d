4bc843e14044985c69155f6719e24801836d0c86
sleepyeye <wonjunlee.0729@gmail.com>
2023-05-27 01:23:10 +0900

    Fix python mode hook for sleepy/python-capf

--- python.el
+++ python.el
@@ -8,21 +8,13 @@
 					 ))))
 
 
-(with-eval-after-load 'python-mode
-  (add-hook 'eglot-managed-mode-hook #'sleepy/python-capf))
-
-(with-eval-after-load 'python-ts-mode
-  (add-hook 'eglot-managed-mode-hook #'sleepy/python-capf))
+(add-hook 'python-mode #'sleepy/python-capf)
+(add-hook 'python-ts-mode #'sleepy/python-capf)
 
 (use-package python
   :after projectile
   :init
-  (setq python-indent-guess-indent-offset-verbose nil)
-  ;; :config
-  ;; (when (and (executable-find "python3")
-  ;; 			 (string= python-shell-interpreter "python"))
-  ;; 	(setq python-shell-interpreter "python3"))
-  )
+  (setq python-indent-guess-indent-offset-verbose nil))
 
 (use-package python-black
   :after python
