531dd6aed36a8735d1c7a21bbe10f7eec18e0d93
sleepyeye <wonjunlee.0729@gmail.com>
2023-08-24 13:36:55 +0900

    Remove org-roam

--- org.el
+++ org.el
@@ -22,34 +22,6 @@
 (use-package valign
   :hook (org-mode . valign-mode))
 
-
-(use-package org-roam
-  :defer 2
-  :custom
-  (org-roam-directory (expand-file-name "~/Dropbox/roam"))
-  :general
-  (sleepy/leader-def
-	"rf" #'org-roam-node-find
-	"ri" #'org-roam-node-insert
-	"rc" #'org-roam-capture
-	"r]" #'org-roam-dailies-capture-today
-	"rt" #'org-roam-dailies-goto-today
-	"rT" #'org-roam-dailies-goto-tomorrow
-	"ry" #'org-roam-dailies-goto-yesterday)
-
-  :config
-  (setq org-roam-completion-everywhere t)
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (setq org-roam-database-connector 'sqlite-builtin)
-  (setq org-roam-dailies-capture-templates
-		'(("d" "default" entry "\n\n* %<%H:%M> %?"
-		   :empty-lines 1
-		   :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n\n\n"))
-		  ))
-  (org-roam-db-autosync-mode))
-
-
 (use-package evil-org
   :ensure t
   :after org
