b09c7f471f41bb29a3545c25ee46cbd94c23f860
sleepyeye <wonjunlee.0729@gmail.com>
2023-09-23 10:31:43 +0900

    Update python.el

--- python.el
+++ python.el
@@ -12,24 +12,18 @@
 (add-hook 'python-ts-mode #'sleepy/python-capf)
 
 (use-package python
+  :elpaca nil
   :init
   (setq python-indent-guess-indent-offset-verbose nil)
   :config
   (when (and (executable-find "python3")
 			 (string= python-shell-interpreter "python"))
-	(setq python-shell-interpreter "python3"))
-  )
+	(setq python-shell-interpreter "python3")))
 
 (use-package python-black
   :after python
-  :hook ((python-mode . python-black-on-save-mode-enable-dwim)
-		 (python-ts-mode . python-black-on-save-mode-enable-dwim))
-  :config
-  (general-evil-define-key '(normal) python-ts-mode-map
-	"=" #'python-black-region)
-
-  (general-evil-define-key '(normal) python-mode-map
-	"=" #'python-black-region))
+  :general
+  (:keymaps 'python-mode-map :states 'normal "=" #'python-black-region))
 
 
 (use-package conda
