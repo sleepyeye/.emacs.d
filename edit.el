6d2a2f91d41443992469088349094359d1cd319d
sleepyeye <wonjunlee.0729@gmail.com>
2023-09-23 10:43:14 +0900

    Clean

--- edit.el
+++ edit.el
@@ -22,8 +22,8 @@
 
 (use-package expand-region
   :bind (("M-=". er/expand-region)
-	 ("M-+". er/expand-region)
-	 ("M--". er/contract-region)))
+		 ("M-+". er/expand-region)
+		 ("M--". er/contract-region)))
 
 
 ;; Configure Tempel
