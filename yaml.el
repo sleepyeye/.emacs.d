fcbdf3cc49a6396e066f0a339a54182c423571e7
sleepyeye <wonjunlee.0729@gmail.com>
2024-02-14 17:02:12 +0900

    Update yaml configs

--- yaml.el
+++ yaml.el
@@ -1,18 +1,19 @@
-;; (elpaca-use-package yaml-pro
-;; 	:hook ((yaml-mode . yaml-pro-mode)
-;; 				 (yaml-ts-mode . yaml-pro-ts-mode))
-;; 	:general
-;; 	(general-evil-define-key '(normal) yaml-pro-ts-mode-map
-;; 		">" #'yaml-pro-ts-indent-subtree
-;; 		"<" #'yaml-pro-ts-unindent-subtree
-;; 		"gh" #'yaml-pro-ts-up-level
-;; 		"gj" #'yaml-pro-ts-next-subtree
-;; 		"gk" #'yaml-pro-ts-prev-subtree)
-
-;; 	(general-define-key
-;; 	 :keymaps 'yaml-pro-ts-mode-map
-;; 	 "M-j" #'yaml-pro-ts-move-subtree-up
-;; 	 "M-k" #'yaml-pro-ts-move-subtree-down)
-;; 	)
+(use-package yaml-mode
+  :mode (("\\.yml" . yaml-mode)
+		 ("\\.yaml" . yaml-mode)))
 
 
+(use-package yaml-pro
+  :after yaml-mode
+  :general
+  (general-evil-define-key '(normal) yaml-pro-ts-mode-map
+	">" #'yaml-pro-ts-indent-subtree
+	"<" #'yaml-pro-ts-unindent-subtree
+	"gh" #'yaml-pro-ts-up-level
+	"gj" #'yaml-pro-ts-next-subtree
+	"gk" #'yaml-pro-ts-prev-subtree)
+
+  (general-define-key
+   :keymaps 'yaml-pro-ts-mode-map
+   "M-j" #'yaml-pro-ts-move-subtree-up
+   "M-k" #'yaml-pro-ts-move-subtree-down))
