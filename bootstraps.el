49a27f55b5d76aa95ab432e4c26fb940d9396804
sleepyeye <wonjunlee.0729@gmail.com>
2024-04-19 21:08:04 +0900

    Update elpaca version

--- bootstraps.el
+++ bootstraps.el
@@ -1,10 +1,10 @@
-(defvar elpaca-installer-version 0.6)
+(defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults (:exclude "extensions"))
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
@@ -13,10 +13,13 @@
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
+                 ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                 ,@(when-let ((depth (plist-get order :depth)))
+                                                     (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                 ,(plist-get order :repo) ,repo))))
                  ((zerop (call-process "git" nil buffer t "checkout"
                                        (or (plist-get order :ref) "--"))))
                  (emacs (concat invocation-directory invocation-name))
@@ -24,7 +27,7 @@
                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
                  ((require 'elpaca))
                  ((elpaca-generate-autoloads "elpaca" repo)))
-            (kill-buffer buffer)
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
           (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
@@ -37,8 +40,18 @@
 ;; Install use-package support
 (elpaca elpaca-use-package
   (elpaca-use-package-mode)
-  (setq elpaca-use-package-by-default t)
-  )
+  (setq elpaca-use-package-by-default t))
+
+;; ;; 'always-defer' means that for a package to load we need a ':hook' or using a ':general' keybinding
+;; ;; if there is none, we need to explicitly add ':demand' to load the package
+;; ;; can also load with ':defer time'
+;; (setq use-package-verbose nil		; don't print anything
+;;       use-package-compute-statistics nil ; compute statistics about package initialization
+;;       use-package-minimum-reported-time 0.0001
+;;       use-package-always-ensure t	; always ensure the package is installed, unless :ensure nil
+;;       use-package-expand-minimally t	; minimal expanded macro
+;; 	  )
+;;       ;; use-package-always-defer t)	; always defer, don't "require", except when :demand
 
 
 ;; built-in features
