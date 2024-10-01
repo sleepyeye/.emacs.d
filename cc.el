e000bc651b8efc4599658fd8714dad898f75e070
sleepyeye <wonjunlee.0729@gmail.com>
2024-10-01 15:59:55 +0900

    clean

--- cc.el
+++ cc.el
@@ -23,30 +23,6 @@
 (use-package c-mode :elpaca nil :disabled t)
 (use-package c++-mode :elpaca nil :disabled t)
 
-;; (use-package cc-mode
-;;   :ensure nil
-;;   :config
-;;   (sleepy/leader-def
-;; 	:keymaps 'c-mode-base-map
-;; 	"SPC" #'projectile-find-other-file
-;; 	"pc"  #'projectile-compile-project
-;; 	"pC"  #'projectile-configure-project)
-;; 	;;; from emacs wiki
-;;   (c-add-style "microsoft"
-;; 			   '("stroustrup"
-;; 				 (c-offsets-alist
-;; 				  (innamespace . -)
-;; 				  (inline-open . 0)
-;; 				  (inher-cont . c-lineup-multi-inher)
-;; 				  (arglist-cont-nonempty . +)
-;; 				  (template-args-cont . +)))))
-
-;; (use-package google-c-style
-;;   :hook ((c-mode c++-mode c-ts-mode c++-ts-mode) . (lambda ()
-;; 													 (google-set-c-style)
-;; 													 (google-make-newline-indent)
-;; 													 (setq c-basic-offset 4))))
-
 (use-package simpc-mode
   :init
   (add-to-list 'major-mode-remap-alist '(c-mode . simpc-mode))
@@ -60,24 +36,9 @@
 (use-package modern-cpp-font-lock
   :load-path local-package-directory
   :config
-  (modern-c++-font-lock-global-mode t))
+  (modern-c++-font-lock-global-mode))
 
 (use-package clang-format+
   :hook ((c-mode-hook . clang-format+-mode)
 		 (c++-mode-hook . clang-format+-mode)
 		 (simpc-mode . clang-format+-mode)))
-
-;; ;;; Currently we only need to guess indent offset in c-c++ modes
-;; (use-package dtrt-indent
-;;   :hook ((c-mode . dtrt-indent-mode)
-;; 		 (c++-mode . dtrt-indent-mode))
-;;   :config
-;;   ;; register c-ts-mode and  c++-ts-mode to dtrt-indent
-;;   (add-to-list 'dtrt-indent-hook-mapping-list
-;; 			   '(c-ts-base-mode c/c++/java c-ts-mode-indent-offset))
-;;   :custom
-;;   ;; dtrt-indent mode will automatically update the listed variables
-;;   ;; note all modes and variables should be registered in dtrt-indent-hook-mapping-list
-;;   (dtrt-indent-hook-generic-mapping-list
-;;    '((evil-mode evil-shift-width)
-;; 	 (c-ts-base-mode c-ts-mode-indent-offset))))
