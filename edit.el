fded3266a2babeb362c90e6838e719cf8716233b
sleepyeye <wonjunlee.0729@gmail.com>
2024-11-03 18:06:04 +0900

    Remove drag-stuff

--- edit.el
+++ edit.el
@@ -1,15 +1,3 @@
-(use-package drag-stuff
-  :demand t
-  :diminish drag-stuff-mode
-  :config
-  (drag-stuff-global-mode t)
-  (general-define-key
-   :keymaps 'override
-   "M-j" #'drag-stuff-down
-   "M-k" #'drag-stuff-up
-   "M-h" #'drag-stuff-left
-   "M-l" #'drag-stuff-right))
-
 (use-package iedit)
 
 (use-package ialign)
