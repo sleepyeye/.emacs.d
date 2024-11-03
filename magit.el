805f3c7eef8fc536dcec04a520ec39d649b973f5
sleepyeye <wonjunlee.0729@gmail.com>
2024-11-03 18:14:39 +0900

    Optimize loading speed of magit related package

--- magit.el
+++ magit.el
@@ -1,13 +1,14 @@
 (use-package transient
-  :demand t
+  :defer t
   :config
   (transient-bind-q-to-quit))
 
 (use-package magit
-  :ensure t
-  :demand t
-  :config
+  :defer t
+  :commands magit-status
+  :general
   (sleepy/leader-def "gg" 'magit-status)
+  :config
   ;; See https://github.com/magit/magit/issues/2124
   (defun mu-magit-kill-buffers (param)
 	"Restore window configuration and kill all Magit buffers."
@@ -18,7 +19,6 @@
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
 		magit-diff-hide-trailing-cr-characters t)
   (magit-auto-revert-mode t)
-  (global-auto-revert-mode t)
   (setq-default magit-bury-buffer-function #'mu-magit-kill-buffers))
 
 (use-package diff-hl
@@ -35,7 +35,6 @@
 
 
 (use-package git-timemachine
+  :defer t
   :commands git-timemachine
-  :config
-  (sleepy/leader-def
-	"gt" 'git-timemachine))
+  :config (sleepy/leader-def "gt" 'git-timemachine))
