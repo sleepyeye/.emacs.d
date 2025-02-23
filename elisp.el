414f5820fdcff28814af711c3601209574077aeb
sleepyeye <wonjunlee.0729@gmail.com>
2025-02-23 15:06:42 +0900

    Update elisp cape-capf-super

--- elisp.el
+++ elisp.el
@@ -2,11 +2,9 @@
   (setq-local completion-at-point-functions
 			  (list (cape-capf-super
 					 ;; for emacs-lisp symbols
-					 #'citre-completion-at-point
-					 #'elisp-completion-at-point
 					 #'cape-elisp-symbol
-					 #'cape-file
-					 ))))
+					 #'cape-file))))
+
 
 ;; elisp mode do not use eglot.
 (add-hook 'emacs-lisp-mode-hook #'sleepy/elisp-capf)
