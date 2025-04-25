598cfe863340ad912199f86176f3819ec2dcb878
sleepyeye <wonjunlee.0729@gmail.com>
2025-04-25 14:59:53 +0900

    Clean

--- ui.el
+++ ui.el
@@ -152,42 +152,6 @@
   (popper-mode +1)
   (popper-echo-mode +1))
 
-;; ;; FIXME
-;; ;; Stolen from @xenodium's config
-;; ;; Welcome buffer
-;; (defun ar/show-welcome-buffer ()
-;;   "Show *Welcome* buffer."
-;;   (with-current-buffer (get-buffer-create "*Welcome*")
-;; 	(setq truncate-lines t)
-;; 	(let* ((buffer-read-only)
-;; 		   (image-path "~/.emacs.d/emacs.png")
-;; 		   (image (create-image image-path))
-;; 		   (size (image-size image))
-;; 		   (height (cdr size))
-;; 		   (width (car size))
-;; 		   (top-margin (floor (/ (- (window-height) height) 2)))
-;; 		   (left-margin (floor (/ (- (window-width) width) 2)))
-;; 		   (prompt-title "Welcome to Emacs!"))
-;; 	  (erase-buffer)
-;; 	  (setq mode-line-format nil)
-;; 	  (goto-char (point-min))
-;; 	  (insert (make-string top-margin ?\n ))
-;; 	  (insert (make-string left-margin ?\ ))
-;; 	  (insert-image image)
-;; 	  (insert "\n\n\n")
-;; 	  (insert (make-string (floor (/ (- (window-width) (string-width prompt-title)) 2)) ?\ ))
-;; 	  (insert prompt-title))
-;; 	(setq cursor-type nil)
-;; 	(read-only-mode +1)
-;; 	(switch-to-buffer (current-buffer))
-;; 	(local-set-key (kbd "q") 'kill-this-buffer)))
-;; (setq initial-scratch-message nil)
-;; (setq inhibit-startup-screen t)
-;; (when (< (length command-line-args) 2)
-;;   (add-hook 'emacs-startup-hook (lambda ()
-;; 								  (when (display-graphic-p)
-;; 									(ar/show-welcome-buffer)))))
-
 (use-package frame
   :ensure nil
   :defer t
