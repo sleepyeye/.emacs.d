155ff0de755de08f32dd55ccae7835288de9ffd4
sleepyeye <wonjunlee.0729@gmail.com>
2025-11-29 08:29:22 +0900

    Enable ai.el loading in init
    - Load AI tools configuration module

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
 (load "~/.emacs.d/media.el")
 (load "~/.emacs.d/tree-sitter.el")
 (load "~/.emacs.d/register.el")
-;; (load "~/.emacs.d/ai.el")
+(load "~/.emacs.d/ai.el")
 
 ;;; Platform-specific configuration
 (when IS-MAC
